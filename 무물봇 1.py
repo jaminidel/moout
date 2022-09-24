8cd9e69565c6e81da8bff066a543461dc33812d3
jaminidel <109906349+jaminidel@users.noreply.github.com>
2022-09-24 18:46:23 +0900

    Update 무물봇 1.py

--- 무물봇 1.py
+++ 무물봇 1.py
@@ -3,7 +3,7 @@ from discord.ui import Button,View
 from discord.ext import commands
 from discord.utils import get
 import asyncio
-
+import os
 bot = commands.Bot(command_prefix="!",intents = discord.Intents.all())
 
 
@@ -50,4 +50,5 @@ async def on_messange(ctx):
     await ctx.message.author.send ("출석체크가 완료 되었습니다!")
     embedl = discord.Embed(title="출석체크 됨",description=(author_name), color=0xf9ff84)
     await chl.send(embed=embedl)
-bot.run('ddddddddddddddddddddddddddddddddddddddddddddddddddddd')
+acsses_token = os.environ["BOT_TOKEN"]
+bot.run(acsses_token)
